c711ab5dd505591fab516f7f3bee6aa3239da7a7
VinodLyric <36466385+VinodLyric@users.noreply.github.com>
2021-08-20 12:12:29 +0530

    Add files via upload

---
+++ docker_instructions.md
@@ -0,0 +1,96 @@
+# Docker Instructions
+
+Why are we running an earlier version of TimescaleDB (2.0.0)? `signal 11: Segmentation fault`: <https://github.com/timescale/timescaledb/issues/2968>
+
+Extract `mosquitto.conf`.
+
+```shell
+docker run --name tmp_mosquitto -p 1883:1883 eclipse-mosquitto:2.0.9
+docker exec -it tmp_mosquitto cat /mosquitto/config/mosquitto.conf eclipse-mosquitto
+docker rm -f tmp_mosquitto
+```
+
+Create directories on Gateway.
+
+```shell
+mkdir -p ~/data/postgres
+mkdir -p ~/data/grafana
+mkdir -p ~/data/mosquitto/config
+mkdir -p ~/data/mosquitto/data
+mkdir -p ~/data/mosquitto/log
+```
+
+Clone git project on Gateway versus copying.
+
+```shell
+git clone --branch v2021-03 --single-branch --depth 1 \
+    https://github.com/garystafford/iot-analytics-at-the-edge.git
+
+cp ./docker/mosquitto.conf ~/data/mosquitto/config/
+
+# EDGE_DEVICE_HOST=192.168.1.12
+# scp -i ~/.ssh/rasppi docker/mosquitto.conf \
+#     pi@${EDGE_DEVICE_HOST}:~/data/mosquitto/config/
+```
+ 
+Deploy stack on Gateway.
+
+```shell
+docker swarm init
+docker stack deploy -c stack.yml iot
+
+docker stack ps iot --no-trunc
+docker stack services iot
+```
+
+Results.
+
+```text
+> docker ps
+
+ID             NAME              MODE         REPLICAS   IMAGE                              PORTS
+c9mwb57p7fvj   iot_grafana       replicated   1/1        grafana/grafana:7.5.2              *:3000->3000/tcp
+4jhihwja0ct6   iot_mosquitto     replicated   1/1        eclipse-mosquitto:2.0.9            *:1883->1883/tcp
+woyaqpiohu7c   iot_pgadmin       replicated   1/1        biarms/pgadmin4:4.21               *:5050->5050/tcp
+ij1huytkfh5c   iot_timescaledb   replicated   1/1        timescale/timescaledb:2.0.0-pg12   *:5432->5432/tcp
+```
+
+```text
+> docker stack services iot
+ID             NAME              MODE         REPLICAS   IMAGE                              PORTS
+c9mwb57p7fvj   iot_grafana       replicated   1/1        grafana/grafana:7.5.2              *:3000->3000/tcp
+4jhihwja0ct6   iot_mosquitto     replicated   1/1        eclipse-mosquitto:2.0.9            *:1883->1883/tcp, *:9001->9001/tcp
+woyaqpiohu7c   iot_pgadmin       replicated   1/1        biarms/pgadmin4:4.21               *:5050->5050/tcp
+ij1huytkfh5c   iot_timescaledb   replicated   1/1        timescale/timescaledb:2.0.0-pg12   *:5432->5432/tcp
+```
+
+Delete the stack.
+
+```shell
+docker stack rm iot
+```
+
+## Troubleshooting
+
+Test Mosquitto directly. Log into two different sessions - sub and pub
+
+```shell
+MOSQUITTO_CONTAINER=$(docker ps -q \
+    --filter='name=iot_mosquitto.1' --format '{{.Names}}')
+docker exec -it ${MOSQUITTO_CONTAINER} sh
+
+mosquitto_sub -d -t sensor/output
+
+mosquitto_pub -d -t sensor/output -m '{"data":{"co":0.004965300196440765,"humidity":null,"light":true,"lpg":0.0
+07661274225855072,"motion":false,"smoke":0.020441056991654268,"temperature":null},"device_id":"b8:27:eb:bf:9d:51","
+time":"2021-03-26 16:03:24.437824+00:00"}'
+
+# confirm messages are flowing
+MOSQUITTO_CONTAINER=$(docker ps -q \
+    --filter='name=iot_mosquitto.1' --format '{{.Names}}')
+docker exec -it ${MOSQUITTO_CONTAINER} mosquitto_sub -h localhost -t sensor/output
+```
+
+## $SYS Topics
+
+<https://github.com/mqtt/mqtt.org/wiki/SYS-Topics>
---
+++ statements.sql
@@ -0,0 +1,183 @@
+CREATE DATABASE demo_iot;
+\c
+
+CREATE EXTENSION IF NOT EXISTS timescaledb CASCADE;
+
+-- iot data table
+CREATE TABLE IF NOT EXISTS sensor_data
+(
+    time        timestamptz      NOT NULL,
+    device_id   text             NOT NULL,
+    temperature double PRECISION NOT NULL,
+    humidity    double PRECISION NOT NULL,
+    lpg         double PRECISION NOT NULL,
+    co          double PRECISION NOT NULL,
+    smoke       double PRECISION NOT NULL,
+    light       boolean          NOT NULL,
+    motion      boolean          NOT NULL
+);
+
+SELECT create_hypertable('sensor_data', 'time');
+
+
+-- create materialized views (continuous aggregates)
+
+-- temperature and humidity
+CREATE MATERIALIZED VIEW temperature_humidity_summary_minute(device_id, bucket, avg_temp, avg_humidity)
+    WITH (timescaledb.continuous) AS
+        SELECT device_id,
+               time_bucket(INTERVAL '1 minute', time),
+               avg(temperature),
+               avg(humidity)
+        FROM sensor_data
+        WHERE humidity BETWEEN 0 AND 100
+        GROUP BY time_bucket(INTERVAL '1 minute', time), device_id
+    WITH NO DATA;
+
+-- air quality (lpg, co, smoke)
+CREATE MATERIALIZED VIEW air_quality_summary_minute(device_id, bucket, avg_lpg, avg_co, avg_smoke)
+    WITH (timescaledb.continuous) AS
+        SELECT device_id,
+               time_bucket(INTERVAL '1 minute', time),
+               avg(lpg),
+               avg(co),
+               avg(smoke)
+        FROM sensor_data
+        GROUP BY time_bucket(INTERVAL '1 minute', time), device_id
+    WITH NO DATA;
+
+-- light
+CREATE MATERIALIZED VIEW light_summary_minute(device_id, bucket, avg_light)
+    WITH (timescaledb.continuous) AS
+        SELECT device_id,
+               time_bucket(INTERVAL '1 minute', time),
+               avg(case when light = 't' then 1 else 0 end)
+        FROM sensor_data
+        GROUP BY time_bucket(INTERVAL '1 minute', time), device_id
+    WITH NO DATA;
+
+-- motion
+CREATE MATERIALIZED VIEW motion_summary_minute(device_id, bucket, avg_motion)
+    WITH (timescaledb.continuous) AS
+        SELECT device_id,
+               time_bucket(INTERVAL '1 minute', time),
+               avg(case when motion = 't' then 1 else 0 end)
+        FROM sensor_data
+        GROUP BY time_bucket(INTERVAL '1 minute', time), device_id
+    WITH NO DATA;
+
+-- view continuous aggregates
+SELECT * FROM timescaledb_information.continuous_aggregates;
+
+
+-- create policies that automatically refreshes continuous aggregates
+SELECT add_continuous_aggregate_policy('air_quality_summary_minute',
+    start_offset => INTERVAL '1 week',
+    end_offset => INTERVAL '1 hour',
+    schedule_interval => INTERVAL '1 hour');
+
+SELECT add_continuous_aggregate_policy('light_summary_minute',
+    start_offset => INTERVAL '1 week',
+    end_offset => INTERVAL '1 hour',
+    schedule_interval => INTERVAL '1 hour');
+
+SELECT add_continuous_aggregate_policy('motion_summary_minute',
+    start_offset => INTERVAL '1 week',
+    end_offset => INTERVAL '1 hour',
+    schedule_interval => INTERVAL '1 hour');
+
+SELECT add_continuous_aggregate_policy('temperature_humidity_summary_minute',
+    start_offset => INTERVAL '1 week',
+    end_offset => INTERVAL '1 hour',
+    schedule_interval => INTERVAL '1 hour');
+
+--view jobs
+SELECT * FROM timescaledb_information.jobs;
+
+--view job stats
+SELECT job_id, total_runs, total_failures, total_successes
+  FROM timescaledb_information.job_stats;
+
+
+-- grafana user and grants
+CREATE USER grafanareader WITH PASSWORD 'grafana1234';
+GRANT USAGE ON SCHEMA public TO grafanareader;
+GRANT SELECT ON public.sensor_data TO grafanareader;
+GRANT SELECT ON public.temperature_humidity_summary_minute TO grafanareader;
+GRANT SELECT ON public.air_quality_summary_minute TO grafanareader;
+GRANT SELECT ON public.light_summary_minute TO grafanareader;
+GRANT SELECT ON public.motion_summary_minute TO grafanareader;
+
+-- query views
+SELECT *
+FROM temperature_humidity_summary_minute
+ORDER BY bucket;
+
+-- ad-hoc queries
+-- find max temperature (°C) and humidity (%) for last 3 hours in 15 minute time periods
+-- https://docs.timescale.com/latest/using-timescaledb/reading-data#select
+SELECT time_bucket('15 minutes', time) AS fifteen_min,
+       device_id,
+       count(time),
+       max(temperature) AS max_temp,
+       max(humidity) AS max_hum
+FROM sensor_data
+WHERE time > now() - INTERVAL '3 hours'
+  AND humidity BETWEEN 0 AND 100
+GROUP BY fifteen_min, device_id
+ORDER BY fifteen_min DESC, max_temp desc;
+
+-- find temperature (°C) anomalies (delta > ~5°F)
+-- https://docs.timescale.com/latest/using-timescaledb/reading-data#delta
+WITH ht AS (SELECT time,
+                   temperature,
+                   abs(temperature - lag(temperature) over (ORDER BY time)) AS delta
+            FROM sensor_data)
+SELECT ht.time, ht.temperature, ht.delta
+FROM ht
+WHERE ht.delta > 2.63
+ORDER BY ht.time;
+
+-- find three minute moving average of temperature (°F) for last day
+-- (5 sec. interval * 36 rows = 3 min.)
+-- https://docs.timescale.com/latest/using-timescaledb/reading-data#moving-average
+SELECT time,
+       avg((temperature * 1.9) + 32) over (ORDER BY time
+           ROWS BETWEEN 35 PRECEDING AND CURRENT ROW)
+           AS smooth_temp
+FROM sensor_data
+WHERE device_id = 'Manufacturing Plant'
+    AND time > now() - INTERVAL '1 day'
+ORDER BY time desc;
+
+-- find average humidity (%) for last 12 hours in 5-minute time periods
+-- https://docs.timescale.com/latest/using-timescaledb/reading-data#time-bucket
+SELECT time_bucket('5 minutes', time) AS time_period,
+       avg(humidity) AS avg_humidity
+FROM sensor_data
+WHERE device_id = 'Main Warehouse'
+  AND humidity BETWEEN 0 AND 100
+  AND time > now() - INTERVAL '12 hours'
+GROUP BY time_period
+ORDER BY time_period desc;
+
+-- calculate histograms of avg. temperature (°F) between 55-85°F in 5°F buckets during last 2 days
+-- https://docs.timescale.com/latest/using-timescaledb/reading-data#histogram
+SELECT device_id,
+       count(time),
+       histogram((temperature * 1.9) + 32, 55.0, 85.0, 5)
+FROM sensor_data
+WHERE temperature IS NOT NULL
+    AND time > now() - INTERVAL '2 days'
+GROUP BY device_id;
+
+-- find average light value for last 90 minutes in 5-minute time periods
+-- https://docs.timescale.com/latest/using-timescaledb/reading-data#time-bucket
+SELECT device_id,
+       time_bucket('5 minutes', time) AS five_min,
+       avg(case when light = 't' then 1 else 0 end) AS avg_light
+FROM sensor_data
+WHERE device_id = 'Manufacturing Plant'
+    AND time > now() - INTERVAL '90 minutes'
+GROUP BY device_id, five_min
+ORDER BY five_min desc;
